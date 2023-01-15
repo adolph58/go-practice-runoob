d66bdbdbc9e2211810737012e5c7ac4a9b4a1239
arte <jaunty5@163.com>
2023-01-15 13:37:34 +0800

    2023-1-15

--- src/function_closures.go
+++ src/function_closures.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+func getSequence() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
 func main() {
-	$END$
+	/* nextNumber 为一个函数， 变量 i 为 0 */
+	nextNumber := getSequence()
+
+	/* 调用 nextNumber 函数， i 变量自增 1 并返回 */
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+
+	/* 创建新的函数 nextNumber1, 并查看结果 */
+	nextNumber1 := getSequence()
+	fmt.Println(nextNumber1())
+	fmt.Println(nextNumber1())
 }
